09c0b26f42856e7887b962aafe05b908f3f85c4e
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2019-11-14 12:23:20 +0000

    Implemented player shooting
    including autofire

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+
+    public float speed = 7.5f;
+
+    public Rigidbody2D theRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move to the right relative to this object i.e Move in the direction I'm facing...
+
+        theRb.velocity = transform.right * speed;
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+
     private Vector2 moveInput;
 
     public Rigidbody2D theRB;
 
     public Transform gunArm;
+
+    public Animator anim;
+
+    public GameObject bulletToFire;
+    public Transform firePoint;
+
+    public float timeBetweenShots;
+    private float shotCounter;
 
 
     // Start is called before the first frame update
@@ -24,17 +33,20 @@ public class PlayerController : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
-        //transform.position += new Vector3(moveInput.x, moveInput.y, 0f) * moveSpeed * Time.deltaTime;
+        // Remove speed boost for diagonal movement (1.41 units per frame)
+        moveInput.Normalize();
+
+        // transform.position += new Vector3(moveInput.x, moveInput.y, 0f) * moveSpeed * Time.deltaTime;
 
         theRB.velocity = moveInput * moveSpeed;
 
         Vector3 mousePos = Input.mousePosition;
 
-        //Calculate where the player is on the screen
+        // Calculate where the player is on the screen
 
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
 
-        //Face the direction the bullet firing,
+        // Face the direction the bullet firing,
 
         if (mousePos.x < screenPoint.x)
         {
@@ -52,6 +64,41 @@ public class PlayerController : MonoBehaviour
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        // Fire bullet
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+
+            if (shotCounter <= 0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+
+
+
+
+
+
+        // Animation
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
 
 
     }
