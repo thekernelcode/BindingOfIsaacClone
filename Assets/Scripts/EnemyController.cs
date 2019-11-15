d327a9357898b9250c858e7db44ed01f131f1c43
TheKernelCode <thekernelcode@gmail.com>
2019-11-15 12:18:15 +0000

    Enemy Firing and bullets
    -also fixed animation for enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -15,6 +15,18 @@ public class EnemyController : MonoBehaviour
 
     public int health = 150;
 
+    public GameObject[] deathSplatters;
+    public GameObject hitEffect;
+
+    public bool shouldShoot;
+
+    public GameObject bullet;
+    public Transform firePoint;
+    public float fireRate;
+    private float fireCounter;
+
+    public SpriteRenderer theBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,18 +36,35 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
+        if (theBody.isVisible)
         {
-            moveDirection = PlayerController.instance.transform.position - transform.position;
-        }
-        else
-        {
-            moveDirection = Vector3.zero;
-        }
+            if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) < rangeToChasePlayer)
+            {
+                moveDirection = PlayerController.instance.transform.position - transform.position;
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
 
-        moveDirection.Normalize();
+            moveDirection.Normalize();
 
-        theRB.velocity = moveDirection * moveSpeed;
+            theRB.velocity = moveDirection * moveSpeed;
+
+            if (shouldShoot)
+            {
+                fireCounter -= Time.deltaTime;
+                if (fireCounter <= 0)
+                {
+                    fireCounter = fireRate;
+                    Instantiate(bullet, firePoint.position, firePoint.rotation);
+                }
+            }
+        }
+        
+
+
+
 
 
 
@@ -55,9 +84,17 @@ public class EnemyController : MonoBehaviour
     {
         health -= damage;
 
+        Instantiate(hitEffect, transform.position, transform.rotation);
+
         if (health <= 0)
         {
             Destroy(gameObject);
+
+            int selectedSplatter = Random.Range(0, deathSplatters.Length);
+
+            int rotation = Random.Range(0, 4);
+
+            Instantiate(deathSplatters[selectedSplatter], transform.position, Quaternion.Euler(0f, 0f, rotation * 90)); // Rotate splatter in 90 increments
         }
     }
 }
